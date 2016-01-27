93db175961e8850e2d67c709cc82c956395554cb
Lukas Horak <lhorak@retia.cz>
2016-01-27 22:42:44 +0100

    Assign ids by `node-uuid` to recipes, add few new recipes (just titles)

--- src/recipes.js
+++ src/recipes.js
@@ -1,6 +1,6 @@
 var recipes = [
   {
-    id: '0cp',
+    id: 'bc91f3a6-ea22-46ba-a88d-56402b2d63a2',
     title: 'Celozrnné palačinky',
     ingredients: [
       {amount: '2 hrnky', name: 'celozrnná špaldová mouka'},
@@ -15,9 +15,8 @@ var recipes = [
     'a smažte, dokud palačinka nebude vypadat pevně (pokud jí budete chtít ' +
     'obrátit dřív, tak hrozíte, že se vás roztrhne), poté obraťte a smažte ' +
     'z druhé strany.'
-  },
-  {
-    id: '1ap',
+  },{
+    id: '9ec989f6-c606-4d3f-9380-f75b036ee0e1',
     title: 'Avokádové pesto',
     ingredients: [
       {amount: '2', name: 'zralá avokáda'},
@@ -31,6 +30,33 @@ var recipes = [
     description: 'Avokádo rozkrojte na poloviny, vydlabejte a vidličkou ' +
       'rozmačkejte dohladka. Zakapejte limetkovou šťávou, přidejte najemno ' +
       'nasekaný česnek a koriandr, přilijte olej a vše důkladně promíchejte.'
+  }, {
+    id: '3e399716-0406-4993-bea9-bc6815faf608',
+    title: 'Bramborové placky plněné houbami s kešu dipem'
+  }, {
+    id: 'c2e76a7f-3402-4015-8298-f88527d4bbe1',
+    title: 'Rýžové závitky s avokádem a tofu'
+  }, {
+    id: 'f3572625-4e24-4bc2-934c-096201478e4a',
+    title: 'Bramborový guláš'
+  }, {
+    id: '86abd33d-60a9-4112-b4c6-72f993f42b10',
+    title: 'Seitanový guláš'
+  }, {
+    id: '2477b44f-eed7-4c55-a238-4d9ac8f20cbe',
+    title: 'Pizza z ovesných vloček'
+  }, {
+    id: 'a3f69ad9-74ac-4923-8526-65ed4641c367',
+    title: 'Vege-tuňákový sendvič'
+  }, {
+    id: '6bf7269a-7063-437f-8590-f0aa0d6c725f',
+    title: 'Dýňový burger'
+  }, {
+    id: '19c1fea9-ed6f-4a7f-9651-57aff0e79284',
+    title: 'Čočkový burger'
+  }, {
+    id: '43e46b20-9d44-4a46-813f-a58dd3171d85',
+    title: 'Houbové rizoto'
   }
 ];
 
